b7d2e9e7154e46edab8c8a281bbb00c40d857e6d
Vanessa M <vanessamontero@Vanessas-MacBook-Pro.local>
2017-12-02 11:36:09 -0800

    added html pages and routes

---
+++ routes/html-routes.js
@@ -0,0 +1,26 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/profile", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/profile.html"));
+  });
+
+
+  app.get("/ratings", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/ratings.html"));
+  });
+
+
+  app.get("/analytics", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/analytics.html"));
+  });
+
+};
