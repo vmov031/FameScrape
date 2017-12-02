e6c9d9f90a77f9a0d5b1a776273d96ff488f2ae7
Vanessa M <vanessamontero@Vanessas-MacBook-Pro.local>
2017-12-02 13:42:30 -0800

    updating sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,12 @@
 CREATE DATABASE famescrape;
 USE famescrape;
 
-CREATE TABLE userinfo (
-id INT AUTO_INCREMENT,
-
-)
+CREATE TABLE user_info(
+id INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(60) NOT NULL,
+age VARCHAR(40) NOT NULL,
+location VARCHAR(60) NOT NULL,
+gender VARCHAR(60),
+race VARCHAR(50),
+PRIMARY KEY (id)
+);
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,4 @@
-<script src="https://www.gstatic.com/firebasejs/4.7.0/firebase.js"></script>
-<script>
-  // Initialize Firebase
+
   var config = {
     apiKey: "AIzaSyCYhqQq_ekhIZeZPe9-kXHsn5SwU3K870U",
     authDomain: "famescrape.firebaseapp.com",
@@ -10,4 +8,69 @@
     messagingSenderId: "1027592950865"
   };
   firebase.initializeApp(config);
-</script>
+
+var provider = new firebase.auth.GoogleAuthProvider();
+    var user;
+
+$(document).ready(function() {
+
+   function login() {
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          console.log(result);
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          user = result.user;
+          //sessionStorage.setItem("user", JSON.stringify(user));
+        }).catch(function(error) {
+          console.log(error);
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+        });
+        // Redirect to profile page after login
+        firebase.auth().onAuthStateChanged(user => {
+          if (user) {
+            window.location = "profile.html?uid=" + user.uid;
+          }
+        });
+      }
+
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          //add new user logged in user to firebase
+          addNewUser(user);
+          $("#button2").html("<a class='nav-link' id='profile-link' href='#'>Profile</a>");
+          //Navigate to profile page
+          $(document).on("click", "#profile-link", function() {
+            window.location = "profile.html?uid=" + user.uid;
+          });
+        }
+      });
+
+      function addNewUser(user) {
+      //create user Object
+      var userObj = {
+        displayName: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+        uid: user.uid,
+        bio: "",
+        personal: "",
+      }
+
+      //check if user is in firebase if not add new user
+      var uniqueUser = firebase.database().ref("users").child(user.uid);
+      uniqueUser.once("value")
+        .then(function(snapshot) {
+          if (!snapshot.exists()) {
+            uniqueUser.set(userObj);
+          }
+
+        });
+
+    };
